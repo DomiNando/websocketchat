76813f3dbcf01c9c6c5e18271904861ddae1e74f
DomiNando <Rho@Robertos-MacBook.local>
2013-11-20 16:30:53 -0400

    Errores!

--- eaaweb-master/chatServer.js
+++ eaaweb-master/chatServer.js
@@ -19,51 +19,51 @@ function ChatServer() {
 
     // configuration an utility variables
     //var util = require('./routes/util');
-    this.util: {
-        test: function(text) {
-            if (/^[\],:{}\s]*$/.test(text.replace(/\\["\\\/bfnrtu]/g, '@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
-                //the json is ok
-                return true;
+    var util = {
+    test: function(text) {
+        if (/^[\],:{}\s]*$/.test(text.replace(/\\["\\\/bfnrtu]/g, '@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
+            //the json is ok
+            return true;
+        }
+        
+        return false;
+    },
+    
+    sendError: function(connection, code, message) {
+        var mes = {
+            "event": "error",
+            "data": {
+                "errorCode": code,
+                "errMessage": message
             }
-            
-            return false;
-        },
-        
-        sendError: function(connection, code, message) {
-            var mes = {
-                "event": "error",
-                "data": {
-                    "errorCode": code,
-                    "errMessage": message
-                }
-            };
-        
-            connection.write(JSON.stringify(mes));
-        },
-        
-        getConnection: function(destination) {
-            for (var user in users) {
-                if (users[user].id === destination) {
-                    return users[user].userconnection;
-                }
+        };
+    
+        connection.write(JSON.stringify(mes));
+    },
+    
+    getConnection: function(destination) {
+        for (var user in users) {
+            if (users[user].id === destination) {
+                return users[user].userconnection;
             }
-        },
-        
-        sendMessage: function(connection, message) {
-            connection.write(JSON.stringify(message));
-        },
-        
-        getUser: function(connection) {
-            for (var user in users) {
-                if (users[user].userconnection == connection) {
-                    console.log("[returning user] ", users[user].userName);
-                    return users[user].userName;
-                }
-            }
-            
-            return false;
         }
     },
+    
+    sendMessage: function(connection, message) {
+        connection.write(JSON.stringify(message));
+    },
+    
+    getUser: function(connection) {
+        for (var user in users) {
+            if (users[user].userconnection == connection) {
+                console.log("[returning user] ", users[user].userName);
+                return users[user].userName;
+            }
+        }
+        
+        return false;
+    }
+};
 
     // initial setup
     var chat_server = sockjs.createServer(sockjs_opts),
