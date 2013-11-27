3db68141708468e5bb105ac9dd9161c31fee1804
Victor Martinez <martinezsantiagovictor@gmail.com>
2013-11-27 00:31:42 -0400

    Document ready fix

--- miChat/js/chat.js
+++ miChat/js/chat.js
@@ -1,8 +1,8 @@
 /*global ChatController, $*/
-(function() {
+$('document').ready(function() {
   'use strict';
   var server = 'http://eaa.ece.uprm.edu:3500/chat';
   var options = null;
 
   var chat = new ChatController(server, options);
-})();
+});
