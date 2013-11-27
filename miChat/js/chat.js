bbec93f33ba354b21b5688a8d7de3d58c6240a5d
Victor Martinez <martinezsantiagovictor@gmail.com>
2013-11-27 03:11:34 -0400

    adding

--- miChat/js/chat.js
+++ miChat/js/chat.js
@@ -1,4 +1,6 @@
 /*global ChatController, $, jQuery*/
+
+// this is incase jQuery hasn't used thed $ or jQuery doesn't exist
 if(!$) {
   $ = jQuery || function(handler) {
     document.onload = function () {
