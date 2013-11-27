1008dbec40a2c80ed299a346f5c4fed885404d68
Victor Martinez <martinezsantiagovictor@gmail.com>
2013-11-26 23:18:03 -0400

    updates

--- miChat/js/chatModels.js
+++ miChat/js/chatModels.js
@@ -1,189 +1,190 @@
 /*global SockJS, Stapes*/
-'use strict';
+(function () {
+  'use strict';
 
-var _db = window.sessionStorage;
-_db.setItem('current_id', '');
+  var _db = window.sessionStorage;
+  _db.setItem('current_id', '');
 
-var ChatModel = Stapes.subclass({
-  
-  constructor: function (server, options, messages) {
-    this.set(messages);
-    this.server = new SockJS(server, null, options);
-    this.onOpen();
-    this.onMessage();
-    this.server.onclose = this.onClose();
+  var ChatModel = Stapes.subclass({
+    constructor: function (server, options, messages) {
+      this.set(messages);
+      this.server = new SockJS(server, null, options);
+      this.onOpen();
+      this.onMessage();
+      this.server.onclose = this.onClose();
 
-    this.available = true;
-  },
+      this.available = true;
+    },
 
-  setDestinationId: function (id) {
-    if (id) {
-      this.destinationId = id;
-      _db.setItem('current_id', id);
-    } else {
-      return this.destinationId || _db.getItem('current_id');
-    }
-  },
-
-  login: function (phoneNumber, userName) { //CAMBIE!!
-    this.username = 'Me';
-    this.phoneNumber = phoneNumber || null;
-    this.nickname = userName; //CAMBIE!!!
-
-    var data = {
-      'event': 'login',
-      'data': {
-        'phonenumber': this.phoneNumber,
-        'nickname': this.nickname, //CAMBIE!!,
-        'available': this.available
+    setDestinationId: function (id) {
+      if (id) {
+        this.destinationId = id;
+        _db.setItem('current_id', id);
+      } else {
+        return this.destinationId || _db.getItem('current_id');
       }
-    };
+    },
 
-    this.sendData(data);
+    login: function (phoneNumber, userName) { //CAMBIE!!
+      this.username = 'Me';
+      this.phoneNumber = phoneNumber || null;
+      this.nickname = userName; //CAMBIE!!!
 
-  },
-
-  onOpen: function () {
-    console.log('[open]');
-    var model = this;
-    this.server.onopen = function () {
-      model.emit('open');
-    };
-  },
-
-  onMessage: function () {
-    var model = this;
-    this.server.onmessage = function (response) {
-
-      if (typeof response === 'object') {
-        var e = JSON.parse(response.data);
-        var event = e.event;
-        var data = e.data;
-
-        switch (event) {
-        case 'user ok':
-          model.emit('user ok');
-          break;
-        case "request ok":
-          model.setDestinationId(data.userId);
-          model.emit('request ok');
-          break;
-
-        case "message":
-          var chatMessage = data.message;
-          var userName = data.userName;
-          var dt = {
-            'message': chatMessage,
-            'user': userName,
-            'class': "external"
-          };
-
-          if (data.be_available) {
-            model.sendData({
-              "event": "set available",
-              "data": {
-                "nickname": this.nickname
-              }
-            });
-
-            this.available = true;
-          } else {
-            this.available = false;
-          }
-
-          console.log('data', dt);
-          model.emit('new message', dt);
-          model.addMessage(dt);
-          break;
-        case "error":
-          var code = data.errorCode;
-          var errorMessage = data.errMessage;
-          model.emit('error', {
-            code: code,
-            message: errorMessage
-          });
-          break;
-        case "new chat":
-          var destination = data.destination;
-          var dt = {
-            "event": "requestChat",
-            "data": {
-              "destination": destination
-            }
-          };
-
-          model.sendData(dt);
-          break;
-        default:
-          model.emit('sosumi');
-          break;
+      var data = {
+        'event': 'login',
+        'data': {
+          'phonenumber': this.phoneNumber,
+          'nickname': this.nickname, //CAMBIE!!,
+          'available': this.available
         }
-      }
-    };
-  },
+      };
 
-  onClose: function () {
-    this.emit('close');
-  },
+      this.sendData(data);
 
-  sendData: function (data) {
-    console.log('calling server');
-    this.server.send(JSON.stringify(data));
-  },
+    },
 
-  close: function () {
-    this.server.close();
-  },
+    onOpen: function () {
+      console.log('[open]');
+      var model = this;
+      this.server.onopen = function () {
+        model.emit('open');
+      };
+    },
 
-  addMessage: function (data) {
-    this.push({
-      'username': data.user,
-      'message': data.message,
-      'date': new Date(),
-      'class': data.class
-    });
-  },
+    onMessage: function () {
+      var model = this;
+      this.server.onmessage = function (response) {
 
-  removeOld: function () {
-    var now = new Date();
-    var max_days = 432000000;
+        if (typeof response === 'object') {
+          var e = JSON.parse(response.data);
+          var event = e.event;
+          var data = e.data;
 
-    this.remove(function (message) {
-      return Math.abs(now - message.date) > max_days ? message : null;
-    });
-  },
+          switch (event) {
+          case 'user ok':
+            model.emit('user ok');
+            break;
+          case "request ok":
+            model.setDestinationId(data.userId);
+            model.emit('request ok');
+            break;
 
-  getMessages: function () {
-    // Let's just return all the messages in the stack
-    return this.filter(function (message) {
-      console.log('rendering');
-      return message;
-    });
-  },
+          case "message":
+            var chatMessage = data.message;
+            var userName = data.userName;
+            var dt = {
+              'message': chatMessage,
+              'user': userName,
+              'class': "external"
+            };
 
-  sendMessage: function (message) {
-    var data = {
-      'event': 'message',
-      'data': {
+            if (data.be_available) {
+              model.sendData({
+                "event": "set available",
+                "data": {
+                  "nickname": this.nickname
+                }
+              });
+
+              this.available = true;
+            } else {
+              this.available = false;
+            }
+
+            console.log('data', dt);
+            model.emit('new message', dt);
+            model.addMessage(dt);
+            break;
+          case "error":
+            var code = data.errorCode;
+            var errorMessage = data.errMessage;
+            model.emit('error', {
+              code: code,
+              message: errorMessage
+            });
+            break;
+          case "new chat":
+            var destination = data.destination;
+            var datum = {
+              "event": "requestChat",
+              "data": {
+                "destination": destination
+              }
+            };
+
+            model.sendData(datum);
+            break;
+          default:
+            model.emit('sosumi');
+            break;
+          }
+        }
+      };
+    },
+
+    onClose: function () {
+      this.emit('close');
+    },
+
+    sendData: function (data) {
+      console.log('calling server');
+      this.server.send(JSON.stringify(data));
+    },
+
+    close: function () {
+      this.server.close();
+    },
+
+    addMessage: function (data) {
+      this.push({
+        'username': data.user,
+        'message': data.message,
+        'date': new Date(),
+        'class': data.class
+      });
+    },
+
+    removeOld: function () {
+      var now = new Date();
+      var max_days = 432000000;
+
+      this.remove(function (message) {
+        return Math.abs(now - message.date) > max_days ? message : null;
+      });
+    },
+
+    getMessages: function () {
+      // Let's just return all the messages in the stack
+      return this.filter(function (message) {
+        console.log('rendering');
+        return message;
+      });
+    },
+
+    sendMessage: function (message) {
+      var data = {
+        'event': 'message',
+        'data': {
+          'message': message,
+          'destinationId': _db.getItem('current_id')
+        }
+      };
+
+
+
+      this.sendData(data);
+
+      var info = {
+        'user': this.username,
         'message': message,
-        'destinationId': _db.getItem('current_id')
-      }
-    };
+        'class': 'own'
+      };
 
+      this.addMessage(info);
+    },
 
-
-    this.sendData(data);
-
-    var info = {
-      'user': this.username,
-      'message': message,
-      'class': 'own'
-    };
-
-    this.addMessage(info);
-  },
-
-  handle: function (response) {
-    this.onMessage(response);
-  }
-});
+    handle: function (response) {
+      this.onMessage(response);
+    }
+  });
+})();
--- miChat/js/chatStore.js
+++ miChat/js/chatStore.js
@@ -1,25 +1,29 @@
-/*global Stapes */
-'use strict';
+(function () {
+  /*global Stapes */
+  'use strict';
 
-var ChatStore = Stapes.subclass({
+  var ChatStore = Stapes.subclass({
     constructor: function() {
-        if (!localStorage in window) {
-            console.log("We can't save this in your browser!!");
-            console.log('Probably IE.');
-        }
+      if (!localStorage in window) {
+        console.log("We can't save this in your browser!!");
+        console.log('Probably IE.');
+      }
+
+      this._db = window.localStorage;
     },
 
     load: function () {
-        var result = window.localStorage['emergency-chat'];
+      var result = this._db.getItem('emergency-chat');
 
-        return result ? JSON.parse(result) : {};
+      return result ? JSON.parse(result) : {};
     },
 
     save: function(data) {
-         while (data.length > 10) {
-            data.splice(0,1);
-        }
+      while (data.length > 10) {
+        data.splice(0,1);
+      }
 
-         window.localStorage['emergency-chat'] = JSON.stringify(data);
+      this._db.setItem('emergency-chat', JSON.stringify(data));
     }
-});
+  });
+})();
