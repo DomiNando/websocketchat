99cdb84cc8127332054c12db09f61f1de3d12e61
Victor Martinez <martinezsantiagovictor@gmail.com>
2013-11-27 19:19:06 -0400

    function blocks

--- miChat/js/chat.js
+++ miChat/js/chat.js
@@ -1,20 +1,5 @@
 /*global ChatController, $, jQuery*/
 
-// this is incase jQuery hasn't used thed $ or jQuery doesn't exist.
-(function () {
-  if(!$) {
-  $ = jQuery || function(handler) {
-    document.onload = function() { pageLoad(handler) };
-  };
-
-  var pageLoad = function (handler) {
-    if (document.readyState === 'complete' && hanlder) {
-      handler();
-    }
-  }
-}
-})();
-
 $(function() {
   'use strict';
   var server = 'http://eaa.ece.uprm.edu:3500/chat';
