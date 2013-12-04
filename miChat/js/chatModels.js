95de3ad91fb69af510e36d8b776ad5bba659a703
Victor Martinez <martinezsantiagovictor@gmail.com>
2013-12-04 01:40:28 -0400

    date

--- miChat/js/chatModels.js
+++ miChat/js/chatModels.js
@@ -166,24 +166,24 @@ var ChatModel = Stapes.subclass({
 
   sendMessage: function (message) {
     var today = new Date();
-          var dd = today.getDate();
-          var mm = today.getMonth() + 1;
-          var yyyy = today.getFullYear();
-          var hr = today.getHours();
-          var mins = today.getMinutes();
-          var secs = today.getSeconds();
-          if (dd < 10) {
-            dd = '0' + dd
-          } 
-          if (mm < 10) {
-            mm = '0' + mm
-          }
-          today = mm + '/' + dd + '/' + yyyy;
-          var fullDate = "at " + dd + "/" + mm + "/" + yyyy + " " + hr + ":" + mins + ":" + secs;
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1;
+    var yyyy = today.getFullYear();
+    var hr = today.getHours();
+    var mins = today.getMinutes();
+    var secs = today.getSeconds();
+    if (dd < 10) {
+      dd = '0' + dd
+    } 
+    if (mm < 10) {
+      mm = '0' + mm
+    }
+    today = mm + '/' + dd + '/' + yyyy;
+    var fullDate = "at " + dd + "/" + mm + "/" + yyyy + " " + hr + ":" + mins + ":" + secs;
     var data = {
       'event': 'message',
       'data': {
-        'message': message + fullDate,
+        'message': message ,
         'destinationId': _db.getItem('current_id')
       }
     };
@@ -194,7 +194,7 @@ var ChatModel = Stapes.subclass({
 
     var info = {
       'user': this.username,
-      'message': message,
+      'message': message + fullDate,
       'class': 'own'
     };
 
