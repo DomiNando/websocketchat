f00c5cbb5f2c62509ed372d2363d2b5e2d2cf2cf
Victor Martinez <martinezsantiagovictor@gmail.com>
2013-12-06 22:28:17 -0400

    fixed scroll height error

--- miChat/js/chatView.js
+++ miChat/js/chatView.js
@@ -54,7 +54,7 @@ ChatView.proto({
     $('#messages').html(html);
 
     // code to scroll the chat down on every new message or render.
-    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    $('#messages').scrollTop($('#messages').height());
   },
 
   show: function () {
